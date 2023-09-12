d7d56634fbe4e68bf72e1b68303b2ef146d83b81
haiha278 <haiha278172@gmail.com>
2023-09-12 21:38:22 +0700

    finish EX1 and create module for EX2

--- src/EX1/src/Main.java
+++ src/EX1/src/Main.java
@@ -2,10 +2,24 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        for (int i = 0; i <= employees.length; i++){
-
+        Employee[] employees = {new Employee("Ha", 1990, 200, "Ha Noi"),
+                new Employee("kiet", 1990, 100, "Ha Noi"),
+                new Employee("Tung", 1990, 150, "Ha Noi"),
+                new Employee("Hien", 1990, 90, "Ha Noi")};
+        Employee employee = null;
+        for (int i = 0; i < employees.length; i++) {
+            for (int j = 1; j < employees.length; j++) {
+                if (employees[j - 1].getSalary() > employees[j].getSalary()) {
+                    employee = employees[j - 1];
+                    employees[j - 1] = employees[j];
+                    employees[j] = employee;
+                }
+            }
         }
-
+        System.out.println("Employee has min salary:");
+        System.out.println(employees[0]);
+        System.out.println();
+        System.out.println("Employee has max salary:");
+        System.out.println(employees[employees.length - 1]);
     }
 }
